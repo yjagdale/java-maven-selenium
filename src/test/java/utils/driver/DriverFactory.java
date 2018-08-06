692df2ef5d94b5caef3102f57f5c56e82472417c
Yashwant Jagdale <yashwant.jagdale@veritas.com>
2018-08-06 12:42:12 +0530

    Driver start update in driver file

--- src/test/java/utils/driver/DriverFactory.java
+++ src/test/java/utils/driver/DriverFactory.java
@@ -5,20 +5,31 @@ import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
 
     // Get a new WebDriver Instance.
     // There are various implementations for this depending on browser. The required browser can be set as an environment variable.
     // Refer http://getgauge.io/documentation/user/current/managing_environments/README.html
-    public static WebDriver getDriver() {
+    public static WebDriver getDriver() throws MalformedURLException {
 
         String browser = System.getenv("BROWSER");
         if (browser == null) {
-            ChromeDriverManager.getInstance().setup();
-            return new ChromeDriver();
+            /*ChromeDriverManager.getInstance().setup();
+            return new ChromeDriver();*/
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--start-maximized");
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+            return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
         }
         switch (browser)
         {
